4a5635c6920f218e3eb7c59d1d9f453b36600ec1
Victor Iberi <iberivictor7@gmail.com>
2021-08-22 19:16:13 +0100

    navbar added

---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import logo from '../default-monochrome.svg'
+//React Fontawesome Import
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+
+const Navbar = () => {
+    return (
+<nav className="nav-background navbar navbar-expand-lg navbar-light">
+  {/* <div className="container"> */}
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo"/></a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <FontAwesomeIcon icon={faBars} style={{color: '#fff'}}/>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav ms-auto">
+        <li className="nav-item">
+          <a className="nav-link" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">About Me</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Services</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Portfolio</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  {/* </div> */}
+</nav>
+    )
+}
+
+export default Navbar
