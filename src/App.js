0f3b2ec4cef89640775b630bf779c88347cd24e9
Victor Iberi <iberivictor7@gmail.com>
2021-08-22 19:06:05 +0100

    packages installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar' //.js not necessary cos webpack already know its a js file
 
 function App() {
   return (
-    <h2>Portfolio baby</h2>
+    <Navbar />
   );
 }
 
