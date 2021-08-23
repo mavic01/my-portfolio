aed85d4a067b6deffce3cf4bbb200df19005f727
Victor Iberi <iberivictor7@gmail.com>
2021-08-23 10:39:22 +0100

    deleted import statement in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar' //.js not necessary cos webpack already know its a js file
 
 function App() {
   return (
